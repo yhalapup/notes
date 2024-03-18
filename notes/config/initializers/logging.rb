78b17445c643722d13d60668753d36eb1f451ea3
Yurii <halapup.yurii@gmail.com>
2024-03-18 19:04:47 +0200

    Do not use mongodb logger for tests

--- notes/config/initializers/logging.rb
+++ notes/config/initializers/logging.rb
@@ -1,5 +1,7 @@
 require_relative "mongodb"
 require_relative "../../app/lib/mongodb_logger"
 
-mongodb_logger = MongodbLogger.new
-Rails.logger.broadcast_to(mongodb_logger)
+unless Rails.env.test?
+  mongodb_logger = MongodbLogger.new
+  Rails.logger.broadcast_to(mongodb_logger)
+end
