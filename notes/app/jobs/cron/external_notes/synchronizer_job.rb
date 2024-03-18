9363d3c7b9174b9c7944ca5691d8a9a200754113
Yurii <halapup.yurii@gmail.com>
2024-03-18 18:46:30 +0200

    Fix warning

--- notes/app/jobs/cron/external_notes/synchronizer_job.rb
+++ notes/app/jobs/cron/external_notes/synchronizer_job.rb
@@ -1,8 +1,8 @@
 class Cron::ExternalNotes::SynchronizerJob < ApplicationJob
   queue_as :high
 
-  retry_on Faraday::TimeoutError, wait: :exponentially_longer, attempts: 11
-  retry_on ActiveRecord::RecordInvalid, wait: :exponentially_longer, attempts: 4
+  retry_on Faraday::TimeoutError, wait: :polynomially_longer, attempts: 11
+  retry_on ActiveRecord::RecordInvalid, wait: :polynomially_longer, attempts: 4
   discard_on ActiveJob::DeserializationError do |_job, error|
     Rails.logger.error "Cron::ExternalNotes::SynchronizerJob raises exception, #{[error, message].join}"
   end
