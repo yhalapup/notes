f7617f646243699adaede023c451973d522c848d
Yurii <halapup.yurii@gmail.com>
2024-03-18 15:32:44 +0200

    Improve resque configuration

--- notes/config/initializers/resque.rb
+++ notes/config/initializers/resque.rb
@@ -1,10 +1,10 @@
 #  Configure resque
 Resque.inline = true if Rails.env.test?
 
-rails_env = ENV['RAILS_ENV'] || 'development'
+rails_env = ENV["RAILS_ENV"] || "development"
 config_file = Rails.root.join("config/redis.yml")
 
-resque_config = YAML::load(ERB.new(IO.read(config_file)).result)
+resque_config = YAML.load(ERB.new(File.read(config_file)).result)
 Resque.redis = resque_config[rails_env]
 
 Resque.logger.level = Logger::INFO
