dcee2ee5d1fc47242e8c8fb57626c15ba418b45a
Yurii <halapup.yurii@gmail.com>
2024-03-25 19:19:57 +0200

    Fix initializer for mongo database

--- notes/config/initializers/mongodb.rb
+++ notes/config/initializers/mongodb.rb
@@ -1,9 +1,11 @@
 require "mongo"
 
-Rails.application.config.mongodb_client =
-  @mongodb_client ||= Mongo::Client.new(
-    [ENV.fetch("MONGO_DATABASE_HOST").to_s],
-    database: ENV.fetch("MONGO_DB").to_s,
-    user: ENV.fetch("MONGO_USER").to_s,
-    password: ENV.fetch("MONGO_PASSWORD").to_s
-  )
+unless Rails.env.test?
+  Rails.application.config.mongodb_client =
+    @mongodb_client ||= Mongo::Client.new(
+      [ENV.fetch("MONGO_DATABASE_HOST").to_s],
+      database: ENV.fetch("MONGO_DB").to_s,
+      user: ENV.fetch("MONGO_USER").to_s,
+      password: ENV.fetch("MONGO_PASSWORD").to_s
+    )
+end
